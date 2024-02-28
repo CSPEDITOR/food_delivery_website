a7cad20e156eefecac776195153f7d6a3cb7bc7e
Chandra <chandrashekharaprasad@gmail.com>
2024-02-28 23:40:31 +0530

    adding php some files

---
+++ repeat/menu.php
@@ -0,0 +1,36 @@
+<header>
+        <section class="navbar">
+            <div class="container">
+                <div class="logo">
+                    <a href="#" title="Logo">
+                        <img src="../imgs/logo.png" alt="Restaurant Logo" class="img-responsive">
+                    </a>
+                </div>
+
+                <div class="search">
+                    <form action="food-search.html" method="POST" class="formm">
+                        <input type="search" name="search" placeholder="Search for Food.." class="btn1" required>
+                        <input type="submit" name="submit" value="Search" class="btn2">
+                    </form>
+                </div>
+
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="#homeid">Home</a>
+                        </li>
+                        <li>
+                            <a href="#category">Categories</a>
+                        </li>
+                        <li>
+                            <a href="#foodid">Foods</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </section>
+    </header>
