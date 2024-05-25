c92ce49ad7bcd1a5b0b58177c7a03e0660cb36ac
Chandra <chandrashekharaprasad@gmail.com>
2024-05-25 14:14:22 +0530

    adding the img option and renameing the image name

--- admin/add-category.php
+++ admin/add-category.php
@@ -11,15 +11,25 @@
                 echo $_SESSION['add'];
                 unset ($_SESSION['add']);
             }
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset ($_SESSION['upload']);
+            }
             
             ?>
 
             <div class="container2">
-                <form action="" method="POST">
+                <form action="" method="POST" enctype="multipart/form-data">
                     <table class="tbladdadmin" >
                         <tr>
                             <td>Title</td>
                             <td><input type="text" placeholder="Enter category title" name="title"></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="file" name="image">
+                            </td>
                         </tr>
                         <tr >
                             <td style="vertical-align: top;">
@@ -68,8 +78,52 @@
                     else{
                         $active = "No";
                     }
+
+                    //check whether the image is selected or not and set the value for image name accordingly
+                    // print_r($_FILES['image']);
+                    // die();
+
+                    if(isset($_FILES['image']['name']))
+                    {
+                        //upload the image
+                        //to upload image we need image name, source path and destination path 
+                        $image_name = $_FILES['image']['name'];
+                        
+                        //auto rename our img
+                        //get the extension of out image (jpg,png,gif,etc) eg, food1.jpg
+                        $ext = end(explode('.',$image_name));
+
+                        // now renaming the image name
+                        $image_name = "Food_Category_".rand(000,999).'.'.$ext;  //eg "food_Category_002.jpg"
+
+                        $source_path = $_FILES['image']['tmp_name'];
+                        $destination_path = "../imgs/category/".$image_name;
+
+                        //finally upload the image
+                        $upload = move_uploaded_file($source_path,$destination_path);
+                        
+                        //check whether the img is uploaded or not 
+                        //if the image is not uploaded then will stop th process and redirect with eorro message
+                        if($upload == false)
+                        {
+                            //set the message
+                            $_SESSION['upload']="Failed to upload image";
+
+                            //redriect to add category page
+                            header('location:'.HOMEURL.'admin/add-category.php');
+                            die();
+
+                        }
+                    }
+                    else{
+                        // dont upload the image and se the name as blank
+                        $image_name = "";
+                    }
+
+                    //create sql query to insert category to database
                     $sql="INSERT INTO tbl_category SET 
                     title='$title',
+                    image_name = '$image_name',
                     featured = '$featured',
                     active = '$active'
                     ";
