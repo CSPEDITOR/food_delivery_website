b862e6c66b75e43d1cacce1d1291b66319d82a17
Chandra <chandrashekharaprasad@gmail.com>
2024-06-10 22:06:18 +0530

    Complete the Dashbord of admin page

--- admin/index.php
+++ admin/index.php
@@ -17,24 +17,77 @@
 ?>
             <div class="container2">
                 <div class="box center">
-                    <h2>2</h2>
+
+                <?php
+                //sql query
+                $sql = "SELECT * FROM tbl_category";
+
+                //execute querry
+                $res = mysqli_query($conn,$sql);
+
+                //count rows
+                $count = mysqli_num_rows($res);
+
+                
+                ?>
+                    <h2><?php echo $count;?></h2>
                     <br>
-                    Home
+                    Categories
                 </div>
                 <div class="box center">
-                    <h2>2</h2>
+
+                <?php
+                //sql query
+                $sql2 = "SELECT * FROM tbl_food";
+
+                //execute querry
+                $res2 = mysqli_query($conn,$sql2);
+
+                //count rows
+                $count2 = mysqli_num_rows($res2);
+
+                
+                ?>
+                    <h2><?php echo $count2; ?></h2>
                     <br>
-                    admin
+                    Foods
                 </div>
                 <div class="box center">
-                    <h2>2</h2>
+                <?php
+                //sql query
+                $sql3 = "SELECT * FROM tbl_order";
+
+                //execute querry
+                $res3 = mysqli_query($conn,$sql3);
+
+                //count rows
+                $count3 = mysqli_num_rows($res3);
+
+                
+                ?>
+                    <h2><?php echo $count3; ?></h2>
                     <br>
-                    Admin
+                    Total Orders
                 </div>
                 <div class="box center">
-                    <h2>2</h2>
+                    <?php
+                    //create sql query to get total revenue generated
+                    //aggregate function in sql
+                    $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Delivered' ";
+
+                    //execute the  query
+                    $res4 = mysqli_query($conn,$sql4);
+
+                    //get the value
+                    $row4= mysqli_fetch_assoc($res4);
+
+                    //get the total revenue
+                    $total_revenue = $row4['Total'];
+
+                    ?>
+                    <h2>Rs.<?php echo $total_revenue; ?></h2>
                     <br>
-                    Admin
+                    Revenue Generated
                 </div>
             </div>
         </div>
